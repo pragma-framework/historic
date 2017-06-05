771ba98cc20f9fc0e2af826c32fdbf8835ef988d
Cyril Rouyer <cyril.rouyer@gmail.com>
2017-06-05 10:11:11 +0200

    Migration following the id strategy

--- db/migrations/20170527144759_add_historic_actions_table.php
+++ db/migrations/20170527144759_add_historic_actions_table.php
@@ -4,37 +4,59 @@ use Phinx\Migration\AbstractMigration;
 
 class AddHistoricActionsTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change(){
-        $t = $this->table('historic_actions', ['id' => false, 'primary_key' => 'id']);
-        $t->addColumn('id', 'char', ['limit' => 23])
-          ->addColumn('historisable_type', 'char', ['limit' => 60])
-          ->addColumn('historisable_id', 'char', ['limit' => 23])
-          ->addColumn('type', 'char', ['limit' => 1, 'default' => 'U'])//U ou D
-          ->addColumn('deleted_name', 'string')
-          ->addColumn('created_at', 'datetime')
-          ->addColumn('created_by', 'string')
-          ->addIndex(['historisable_type', 'historisable_id'])
-          ->create();
-    }
+		/**
+		 * Change Method.
+		 *
+		 * Write your reversible migrations using this method.
+		 *
+		 * More information on writing migrations is available here:
+		 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+		 *
+		 * The following commands can be used in this method and Phinx will
+		 * automatically reverse them when rolling back:
+		 *
+		 *    createTable
+		 *    renameTable
+		 *    addColumn
+		 *    renameColumn
+		 *    addIndex
+		 *    addForeignKey
+		 *
+		 * Remember to call "create()" or "update()" and NOT "save()" when working
+		 * with the Table class.
+		 */
+		public function change(){
+			if(defined(ORM_ID_AS_UID) && ORM_ID_AS_UID){
+				$strategy = defined('ORM_UID_STRATEGY') && ORM_UID_STRATEGY == 'mysql' ? 'mysql' : 'php';
+				$t = $this->table('historic_actions', ['id' => false, 'primary_key' => 'id']);
+				switch($strategy){
+					case 'mysql':
+						$t->addColumn('id', 'char', ['limit' => 36])
+						  ->addColumn('historisable_type', 'char', ['limit' => 60])
+							->addColumn('historisable_id', 'char', ['limit' => 36])
+							->addColumn('historisable_ref_type', 'char', ['limit' => 60, 'null' => true])
+          		->addColumn('historisable_ref_id', 'char', ['limit' => 36, 'null' => true]);
+						break;
+					default:
+					case 'php':
+						$t->addColumn('id', 'char', ['limit' => 23])
+							->addColumn('historisable_type', 'char', ['limit' => 60])
+							->addColumn('historisable_id', 'char', ['limit' => 23])
+							->addColumn('historisable_ref_type', 'char', ['limit' => 60, 'null' => true])
+          		->addColumn('historisable_ref_id', 'char', ['limit' => 23, 'null' => true]);
+						break;
+				}
+			}
+			else{
+				$t = $this->table('historic_actions');
+			}
+
+			$t->addColumn('type', 'char', ['limit' => 1, 'default' => 'U'])//U ou D
+				->addColumn('deleted_name', 'string')
+				->addColumn('created_at', 'datetime')
+				->addColumn('created_by', 'string')
+				->addIndex(['historisable_type', 'historisable_id'])
+				->addIndex(['historisable_ref_type', 'historisable_ref_id'])
+				->create();
+		}
 }
