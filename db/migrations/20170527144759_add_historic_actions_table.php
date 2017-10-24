5297ade75a424d9a33fa4cfd726dc906802c55e2
Cyril Rouyer <cyril@netlor.fr>
2017-10-24 18:12:52 +0200

    Fix syntax error

--- db/migrations/20170527144759_add_historic_actions_table.php
+++ db/migrations/20170527144759_add_historic_actions_table.php
@@ -26,7 +26,7 @@ class AddHistoricActionsTable extends AbstractMigration
 		 */
 		public function change(){
 			if(defined('ORM_ID_AS_UID') && ORM_ID_AS_UID){
-				$strategy = ! defined('ORM_UID_STRATEGY') ? 'php' : && ORM_UID_STRATEGY;
+				$strategy = ! defined('ORM_UID_STRATEGY') ? 'php' : ORM_UID_STRATEGY;
 				$t = $this->table('historic_actions', ['id' => false, 'primary_key' => 'id']);
 				switch($strategy){
 					case 'mysql':
