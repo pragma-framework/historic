bab9cb0cc636237eda2f863a9614faca091f35c9
Cyril Giuliani <giuliani.cyril@gmail.com>
2018-12-19 11:30:55 +0100

    update indentations
    #<type>(<portée>): <sujet>
    #
    #<description>
    #
    #<footer>
    #
    # Type: définit le type de commit
    #	build: Système de build (example : gulp, webpack, npm)
    #	ci: Intégration continue (example scopes: Travis, Circle, BrowserStack, SauceLabs)
    #	docs: Documentation
    #	feat: Ajout d'une fonctionnalité
    #	fix: Correction de bogue
    #	perf: Amélioration des performances
    #	refactor: Changement du code qui ne change rien au fonctionnement
    #	style: Changement du style du code (sans changer la logique)
    #	test: Modification des tests
    # Portée: définit quelle partie de votre librairie / application est affectée par le commit (cette information est optionnelle)
    # Sujet: contient une description succinte des changements (utiliser l'impératif présent, pas de majuscule et de point à la fin
    # Description: détailler plus en profondeur les motivations derrière le changement. Même règles que la partie Sujet.
    # Footer: contient les changements importants (Breaking Changes) et les références à des issues GitHub / GitLab ou autre.

--- Pragma/Historic/Historisable.php
+++ Pragma/Historic/Historisable.php
@@ -62,9 +62,9 @@ trait Historisable{
 	protected function buildAction($type){
 		$params = [
 			'historisable_type' => get_class($this),
-			'historisable_id' 	=> $this->id,
-			'type'				=> $type,
-			'global_name' 		=> $this->get_initial_global_name(),
+			'historisable_id'   => $this->id,
+			'type'              => $type,
+			'global_name'       => $this->get_initial_global_name(),
 		];
 		if(strtoupper($type) == 'D'){
 			$params['deleted_name'] = $this->get_global_name();
@@ -73,19 +73,19 @@ trait Historisable{
 	}
 
 	protected function buildHistoRef(Action $action){
-		if (!empty($this->histo_ref)) {
-			foreach ($this->histo_ref as $ref) {
-                $obj = Reference::build([
-                    'action_id' => $action->id,
+	    if (!empty($this->histo_ref)) {
+	        foreach ($this->histo_ref as $ref) {
+	            $obj = Reference::build([
+	                'action_id' => $action->id,
                     'ref_type'  => get_class($ref),
                     'ref_id'    => $ref->id,
                 ]);
-                if (method_exists($ref, 'get_initial_global_name')){
-                    $obj->ref_global_name = $ref->get_initial_global_name();
-                }
-                $obj->save();
-			}
-		}
+	            if (method_exists($ref, 'get_initial_global_name')){
+	                $obj->ref_global_name = $ref->get_initial_global_name();
+	            }
+	            $obj->save();
+	        }
+	    }
 	}
 
 	public function setActionClassname($classname) {
